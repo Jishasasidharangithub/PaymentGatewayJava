590d5ac67c6bb4aad0f8efbec62e8299bdf01853
meridian <riyas@meridian.net.in>
2023-08-22 12:03:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/activity/StatusActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+        Intent mainIntent = getIntent();
+        TextView tv4 = (TextView) findViewById(R.id.textView1);
+        tv4.setText(mainIntent.getStringExtra("transStatus"));
+    }
+
+    public void showToast(String msg) {
+        Toast.makeText(this, "Toast: " + msg, Toast.LENGTH_LONG).show();
+    }
+}
